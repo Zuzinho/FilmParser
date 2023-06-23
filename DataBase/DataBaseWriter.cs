0be3b1db40ec0c544d7946afb56705785537cb85
Никита Зозуля <nik.zozulya.04@mail.ru>
2023-06-23 15:42:39 +0300

    Added mrthods UpdateData and private method ExecuteNonQuery

--- DataBase/DataBaseWriter.cs
+++ DataBase/DataBaseWriter.cs
@@ -3,20 +3,31 @@ using System.Data.SqlClient;
 
 namespace FilmParser.DataBase
 {
-    internal class DataBaseWriter: DataBase
+    internal class DataBaseWriter : DataBase
     {
-        public void InsertData(ISqlConverter modelObject)
+        public static void InsertData(ISqlConverter modelObject)
         {
-            string sqlString = $"INSERT INTO {modelObject.GetTableName()} " +
-                $"{modelObject.GetVariablesString()} VALUES {modelObject.GetValuesString()}";
+            string sqlString = $"INSERT INTO {modelObject.GetTableName()} {modelObject.GetValuesString()}";
 
-            using (connection)
+            ExecuteNonQuery(sqlString);
+        }
+
+        public static void UpdateData(ISqlConverter modelObject)
+        {
+            string sqlString = $"UPDATE {modelObject.GetTableName()} {modelObject.GetSetString()} WHERE {modelObject.GetIdCondition()}";
+
+            ExecuteNonQuery(sqlString);
+        }
+
+
+        private static void ExecuteNonQuery(string sqlString)
+        {
+            using (Connection)
             {
-                connection.Open();
-                SqlCommand sqlCommand = new SqlCommand(sqlString, connection);
+                Connection.Open();
+                SqlCommand sqlCommand = new SqlCommand(sqlString, Connection);
                 sqlCommand.ExecuteNonQuery();
             }
-            
         }
     }
 }

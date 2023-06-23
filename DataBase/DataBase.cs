a5dc095f501f70f56fa61aad73f6dbe2d85a7850
Никита Зозуля <nik.zozulya.04@mail.ru>
2023-06-23 13:55:45 +0300

    Created DataBase class for storing common methhhods and variaties

---
+++ DataBase/DataBase.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.SqlClient;
+
+namespace FilmParser.DataBase
+{
+    internal class DataBase
+    {
+        private string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\user\\source\\repos\\FilmParser\\FilmParser\\AppData\\CinemaDB.mdf;Integrated Security=True";
+        protected SqlConnection connection => new SqlConnection(connectionString);
+
+        
+    }
+}

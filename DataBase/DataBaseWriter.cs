36d622d525fc5bb346dc7bae6c5d45aa55b7e220
Никита Зозуля <nik.zozulya.04@mail.ru>
2023-06-23 13:57:11 +0300

    Created class for writing data to database and created method Insert

---
+++ DataBase/DataBaseWriter.cs
@@ -0,0 +1,22 @@
+﻿using FilmParser.Model;
+using System.Data.SqlClient;
+
+namespace FilmParser.DataBase
+{
+    internal class DataBaseWriter: DataBase
+    {
+        public void InsertData(ISqlConverter modelObject)
+        {
+            string sqlString = $"INSERT INTO {modelObject.GetTableName()} " +
+                $"{modelObject.GetVariablesString()} VALUES {modelObject.GetValuesString()}";
+
+            using (connection)
+            {
+                connection.Open();
+                SqlCommand sqlCommand = new SqlCommand(sqlString, connection);
+                sqlCommand.ExecuteNonQuery();
+            }
+            
+        }
+    }
+}

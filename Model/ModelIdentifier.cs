e6a9b69cc2e687a9748398ccad6f8886567a8b4e
Никита Зозуля <nik.zozulya.04@mail.ru>
2023-06-25 00:43:32 +0300

    Created new class, moved methods for recognasing tables names and id names for specific class

---
+++ Model/ModelIdentifier.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FilmParser.Model
+{
+    internal static class ModelIdentifier
+    {
+        public static string GetTableName<T>() where T : ISqlConverter
+        {
+            Type type = typeof(T);
+            if (type == typeof(Cinema)) return "Cinemas";
+            if (type == typeof(Film)) return "Films";
+            if (type == typeof(Session)) return "Sessions";
+
+            return string.Empty;
+        }
+
+        public static string GetIdName<T>() where T : ISqlConverter
+        {
+            Type type = typeof(T);
+            if (type == typeof(Cinema)) return "CinemaId";
+            if (type == typeof(Film)) return "FilmId";
+            if (type == typeof(Session)) return "SessionId";
+
+            return string.Empty;
+        }
+    }
+}

00588fa50cd999e23b6a87d8b5060175d4452263
Никита Зозуля <nik.zozulya.04@mail.ru>
2023-06-24 23:55:29 +0300

    Created new static class for creating object specific class

---
+++ Model/ModelFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace FilmParser.Model
+{
+    internal static class ModelFactory
+    {
+        public static ISqlConverter GetModelObject<T>(SqlDataReader reader)
+        {
+            Type type = typeof(T);
+            if (type == typeof(Cinema)) return new Cinema(reader);
+            if (type == typeof(Film)) return new Film(reader);
+            if (type == typeof(Session)) return new Session(reader);
+
+            return null;
+        }
+    }
+}

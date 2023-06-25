b06a0dc578740eaf19fe9b767fec618977a0ad63
Никита Зозуля <nik.zozulya.04@mail.ru>
2023-06-26 00:31:13 +0300

    Added parsing methods

--- Parser/Interface/IParser.cs
+++ Parser/Interface/IParser.cs
@@ -1,6 +1,13 @@
-﻿namespace FilmParser.Parser.Interface
+﻿using FilmParser.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FilmParser.Parser.Interface
 {
     internal interface IParser
     {
+        List<Cinema> ParseCinemas();
+        Task<List<Film>> ParseFilms(int cinemaId);
+        Task<List<Session>> ParseSessions(int cinemaId, int filmId);
     }
 }

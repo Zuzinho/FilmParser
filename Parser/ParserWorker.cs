efc3f84d9af5c5dcf51d06a67725df3d24fc13ed
Никита Зозуля <nik.zozulya.04@mail.ru>
2023-06-26 00:32:49 +0300

    Added async method Work, whitch parsing data and inserting it in db

--- Parser/ParserWorker.cs
+++ Parser/ParserWorker.cs
@@ -1,6 +1,38 @@
-﻿namespace FilmParser.Parser
+﻿using FilmParser.DataBase;
+using FilmParser.Model;
+using FilmParser.Parser.Interface;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FilmParser.Parser
 {
     internal class ParserWorker
     {
+        public void Work(IParser parser)
+        {
+            Parallel.ForEach(
+                parser.ParseCinemas(),
+                async cinema =>
+                {
+                    await DataBaseWriter.InsertDataAsync(cinema);
+                    List<Film> films = await parser.ParseFilms(cinema.Id);
+                    Parallel.ForEach(
+                        films,
+                        async film =>
+                        {
+                            await DataBaseWriter.InsertDataAsync(film);
+                            List<Session> sessions = await parser.ParseSessions(cinema.Id, film.Id);
+                            Parallel.ForEach(
+                                sessions,
+                                async session =>
+                                {
+                                    await DataBaseWriter.InsertDataAsync(session);
+                                }
+                            );
+                        }
+                    );
+                }
+            );
+        }
     }
 }

860eaaf5618d5b2e9bbd30b140ace90ad458cc8b
Никита Зозуля <nik.zozulya.04@mail.ru>
2023-06-29 18:08:30 +0300

    Changed returnable type as Task<previous type>

--- Parser/Interface/IParser.cs
+++ Parser/Interface/IParser.cs
@@ -1,13 +1,17 @@
 ﻿using FilmParser.Model;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System;
 
 namespace FilmParser.Parser.Interface
 {
     internal interface IParser
     {
-        List<Cinema> ParseCinemas();
-        Task<List<Film>> ParseFilms(int cinemaId);
-        Task<List<Session>> ParseSessions(int cinemaId, int filmId);
+        IParserSettings parserSettings { get; }
+        HtmlLoader HtmlLoader { get; }
+
+        Task<List<int>> ParseCinemas();
+        Task<List<int>> ParseFilms(int cinemaId);
+        Task<List<int>> ParseSessions(int cinemaId, int filmId);
     }
 }

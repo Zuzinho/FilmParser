ca2e4a1a9b57bef363945ef28bfb87e8719a50cc
Никита Зозуля <nik.zozulya.04@mail.ru>
2023-06-29 18:01:13 +0300

    Added new method for getting NewId column from table TableIds

--- DataBase/DataBaseReader.cs
+++ DataBase/DataBaseReader.cs
@@ -9,13 +9,13 @@ namespace FilmParser.DataBase
     {
         public async static Task<T> SelectAsync<T>(int id) where T : ISqlConverter
         {
-            string sqlString = $"SELECT * FROM {ModelIdentifier.GetTableName<T>()} " +
+            string sqlString = $"SELECT * FROM {TableNamesPairs[typeof(T)]} " +
                 $"WHERE Id = {id}";
             T modelObject = default;
 
             var reader = await GetReaderAsync(sqlString);
 
-            if(reader.Read()) modelObject = (T)(ModelFactory.GetModelObject<T>(reader));
+            if(reader.Read()) modelObject = (T)ModelFactory.GetModelObject<T>(reader);
 
             Connection.Close();
 
@@ -25,18 +25,26 @@ namespace FilmParser.DataBase
         public async static Task<List<T>> SelectAsync<T>(string conditions = "") where T : ISqlConverter
         {
             if (!conditions.Equals("")) conditions = "WHERE " + conditions;
-            string sqlString = $"SELECT * FROM {ModelIdentifier.GetTableName<T>()} {conditions}";
+            string sqlString = $"SELECT * FROM {TableNamesPairs[typeof(T)]} {conditions}";
             List<T> modelObjects = new List<T>();
 
             var reader = await GetReaderAsync(sqlString);
 
-            while (reader.Read()) modelObjects.Add((T)(ModelFactory.GetModelObject<T>(reader)));
+            while (reader.Read()) modelObjects.Add((T)ModelFactory.GetModelObject<T>(reader));
 
             Connection.Close();
 
             return modelObjects;
         }
 
+        internal static async Task<int> GetNewIdAsync(string tableName)
+        {
+            string sqlString = $"SELECT NewId FROM {TableNamesPairs[typeof(int)]} WHERE TableName = '{tableName}'";
+
+            var reader = await GetReaderAsync(sqlString);
+
+            return await reader.ReadAsync() ? reader.GetInt32(0) : -1;
+        }
 
         private async static Task<SqlDataReader> GetReaderAsync(string sqlString)
         {

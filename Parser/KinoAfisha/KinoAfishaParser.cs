cc83523e7466afd29ef9e0299862d409c27b71cd
Никита Зозуля <nik.zozulya.04@mail.ru>
2023-06-29 18:13:10 +0300

    Created classes for parsing KinoAfisha site

---
+++ Parser/KinoAfisha/KinoAfishaParser.cs
@@ -0,0 +1,63 @@
+﻿using FilmParser.Model;
+using FilmParser.Parser.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AngleSharp.Html.Dom;
+using FilmParser.DataBase;
+using System.Text.Json;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace FilmParser.Parser.KinoAfisha
+{
+    internal class KinoAfishaParser : IParser
+    {
+        public IParserSettings parserSettings => new KinoAfishaParserSettings();
+
+        public HtmlLoader HtmlLoader => new HtmlLoader(parserSettings);
+
+        private Dictionary<int, int> _cinemaIdPairs = new Dictionary<int, int>();
+        private Dictionary<int, int> _filmIdPairs = new Dictionary<int, int>();
+
+        public async Task<List<int>> ParseCinemas()
+        {
+            IHtmlDocument htmlDocument = HtmlLoader.GetCinemasSourceAsync().Result;
+            List<int> cinemaIds = new List<int>();
+
+            var cinemaListItems = htmlDocument.GetElementsByClassName("cinemaList_item");
+            foreach(var cinemaListItem in cinemaListItems)
+            {
+                string name = cinemaListItem.GetElementsByClassName("cinemaList_name")[0].TextContent.Trim();
+                string dataParamString = cinemaListItem.GetAttribute("data-param");
+                string uid = JsonSerializer.Deserialize<DataParam>(dataParamString).uid;
+                
+                int cinemaId = await DataBaseWriter.InsertCinemaAsync(name);
+                _cinemaIdPairs.Add(cinemaId, Convert.ToInt32(uid));
+                cinemaIds.Add(cinemaId);
+            }
+
+            return cinemaIds;
+        }
+
+        public async Task<List<int>> ParseFilms(int cinemaId)
+        {
+            IHtmlDocument htmlDocument = HtmlLoader.GetFilmsSourceAsync(cinemaId).Result;
+            List<int> filmIds = new List<int>();
+
+        }
+
+        public async Task<List<int>> ParseSessions(int cinemaId, int filmId)
+        {
+
+        }
+
+        private class DataParam
+        {
+            internal string type;
+            internal string uid;
+        }
+    }
+}
---
+++ Parser/KinoAfisha/KinoAfishaParserSettings.cs
@@ -0,0 +1,19 @@
+﻿using FilmParser.Parser.Interface;
+
+namespace FilmParser.Parser.KinoAfisha
+{
+    internal class KinoAfishaParserSettings : IParserSettings
+    {
+        public string BaseUrl => "https://www.kinoafisha.info/";
+
+        public string CinemasUrlPattern => "cinema/";
+
+        public string FilmsUrlPattern => CinemaUrlPattern + "schedule/";
+
+        public string SessionsUrlPattern => FilmsUrlPattern;
+
+        public string CinemaUrlPattern => "cinema/{cinemaId}/";
+
+        public string FilmUrlPattern => "movies/{filmId}/";
+    }
+}

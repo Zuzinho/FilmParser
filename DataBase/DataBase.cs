3e55bf7e1407e743fec6ecfee899168cdede9946
Никита Зозуля <nik.zozulya.04@mail.ru>
2023-06-29 17:56:03 +0300

    Added new dictionary with type of model as key and name of table as value

--- DataBase/DataBase.cs
+++ DataBase/DataBase.cs
@@ -1,5 +1,8 @@
 ﻿using System.Data.SqlClient;
 using System.Configuration;
+using System.Collections.Generic;
+using System;
+using FilmParser.Model;
 
 namespace FilmParser.DataBase
 {
@@ -8,5 +11,13 @@ namespace FilmParser.DataBase
         private static readonly string _connectionString = ConfigurationManager.ConnectionStrings["CinemaDB"].ConnectionString;
         
         protected static SqlConnection Connection = new SqlConnection(_connectionString);
+
+        protected static Dictionary<Type, string> TableNamesPairs = new Dictionary<Type, string>()
+        {
+            {typeof(Cinema), "Cinemas" },
+            {typeof(Film), "Films" },
+            {typeof(Session), "Sessions" },
+            {typeof(int), "TablesId" }
+        };
     }
 }
